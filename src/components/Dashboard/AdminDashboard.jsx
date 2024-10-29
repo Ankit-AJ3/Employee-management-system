ff03d3082ed5599c007310607d3c37cc6d5e431d
Ankit Jangid <ankitjangid8501@gmail.com>
2024-10-29 14:12:36 +0530

    commit

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Header from '../others/Header'
 
 const AdminDashboard = () => {
   return (
-    <div>
-      
-        AdminDashboard
-      
+    <div className='h-screen w-full p-10'>
+        <Header/>     
+        <div> 
+          <from>
+              <div>
+                  <h3>Task Title</h3>
+                  <input type="text" placeholder='Make a UI Design'/>
+              </div>
+             <div>
+                    <h3>Description</h3>
+                    <textarea name=" " id='' cols="" rows=""> </textarea>
+             </div>
+             <div>
+                    <h3>Date</h3>
+                    <input type='date'/>
+             </div>
+             <div>
+             <h3>Assign to </h3>
+             <input type="text" placeholder='employee name'/>
+             </div>
+              <div>
+              <h3>Category</h3>
+              <input type="text" placeholder='design, dev, etc' />
+              </div>
+              <button>Create task</button>
+          </from>
+        </div> 
     </div>
   )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -8,8 +8,8 @@ const TaskList = () => {
                     <h3 className='bg-red-600 text-sm px-3 py-1 rounded' >High</h3>
                     <h4 className='text-sm'>29 April 2024</h4>
             </div>
-            <h2 className='mt-5 text-xl font-semibold'>Make a Youtube Videos</h2>
-            <p className='text-sm'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam nunc, auctor quis lobortis nec, viverra vel erat. Aenean erat massa, dictum non ligula et, rutrum vulputate eros.</p>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Videos</h2>
+            <p className='text-sm mt-2'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam nunc, auctor quis lobortis nec, viverra vel erat. Aenean erat massa, dictum non ligula et, rutrum vulputate eros.</p>
         </div>
     </div>
   )
