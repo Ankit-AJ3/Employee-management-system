663d5fbe292be033faa1845e3753d544f49ddc71
Ankit Jangid <ankitjangid8501@gmail.com>
2024-10-29 20:13:43 +0530

    commit

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -5,33 +5,34 @@ const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-10'>
         <Header/>     
-        <div> 
-          <from>
-              <div>
-                  <h3>Task Title</h3>
-                  <input type="text" placeholder='Make a UI Design'/>
+          <div> 
+               <form className="flex w-full bg-red-200 items-start justify-between">
+              <div className='w1/2'>
+                     <div>
+                        <h3>Task Title</h3>
+                        <input type="text" placeholder='Make a UI Design'/>
               </div>
-             <div>
-                    <h3>Description</h3>
-                    <textarea name=" " id='' cols="" rows=""> </textarea>
-             </div>
-             <div>
+              <div>
                     <h3>Date</h3>
                     <input type='date'/>
-             </div>
-             <div>
-             <h3>Assign to </h3>
-             <input type="text" placeholder='employee name'/>
-             </div>
-              <div>
-              <h3>Category</h3>
-              <input type="text" placeholder='design, dev, etc' />
               </div>
+              <div>
+                  <h3>Assign to </h3>
+                  <input type="text" placeholder='employee name'/>
+              </div>
+              <div>
+                  <h3>Category</h3>
+                  <input type="text" placeholder='design, dev, etc' />
+              </div>
+          </div>
+          <div className='w1/2'>
+                 <h3>Description</h3>
+                 <textarea name=" " id='' cols="" rows=""> </textarea>
+            </div>   
               <button>Create task</button>
-          </from>
+          </form>   
         </div> 
     </div>
   )
 }
-
 export default AdminDashboard
